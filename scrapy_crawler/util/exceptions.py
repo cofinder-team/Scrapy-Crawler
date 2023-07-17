bed556611a0a541fbc116b29c3c70e8e2b69aad9
황규도 <hgdkim2@naver.com>
2023-07-17 12:07:16 +0900

    style: 함수 형태로 분리

--- scrapy_crawler/util/exceptions.py
+++ scrapy_crawler/util/exceptions.py
@@ -6,7 +6,12 @@ class DropAndAlert(DropItem):
     def __init__(self, item, message="Custom Exception occurred"):
         self.item = item
         self.message = message
+
+        super().__init__(self.message)
+        self._notify_message(item)
+
+    def _notify_message(self, item):
         LabelingSlackBot().post_fail_message(
             item['id'], item['title'], item['source'], item['url'], self.message
         )
-        super().__init__(self.message)
+
