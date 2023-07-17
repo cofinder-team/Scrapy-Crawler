95dec0815e8b3af61a873c7e17df69ab9d9e6ed1
황규도 <hgdkim2@naver.com>
2023-07-17 10:43:45 +0900

    style: remove default comments

--- scrapy_crawler/middlewares.py
+++ scrapy_crawler/middlewares.py
@@ -1,8 +1,3 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 from scrapy import signals
 
 # useful for handling different item types with a single interface
