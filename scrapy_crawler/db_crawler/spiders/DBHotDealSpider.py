eddf882c614a55f8537f578decc72ab89374f6a9
황규도 <hgdkim2@naver.com>
2023-07-19 09:45:17 +0900

    feat: cache with item/option/unused category

--- scrapy_crawler/db_crawler/spiders/DBHotDealSpider.py
+++ scrapy_crawler/db_crawler/spiders/DBHotDealSpider.py
@@ -18,6 +18,7 @@ class DBHotDealSpider(scrapy.Spider):
         super().__init__(**kwargs)
         self.conn = PostgresClient()
         self.cur = self.conn.getCursor()
+        self.cache = dict()
 
     def get_classified_items(self):
         self.cur.execute("SELECT * "
@@ -31,17 +32,40 @@ class DBHotDealSpider(scrapy.Spider):
         unclassified_items = self.get_classified_items()
 
         for row in unclassified_items:
-            logging.log(logging.INFO,
-                        f"https://dev-api.macguider.io/price/deal/{row[9]}/{row[10]}?unused={'true' if row[13] else 'false'}")
-            yield scrapy.Request(
-                url=f"https://dev-api.macguider.io/price/deal/{row[9]}/{row[10]}?unused={'true' if row[13] else 'false'}",
-                callback=self.parse, meta={'db_item': row})
+            if (row[9], row[10], row[13]) in self.cache:
+                logging.info(f"Cache hit: {row[9]}, {row[10]}, {row[13]}")
+                yield DBItemPrice(
+                    id=row[0],
+                    writer=row[1],
+                    title=row[2],
+                    content=row[3],
+                    price=row[4],
+                    source=row[5],
+                    date=row[6],
+                    url=row[7],
+                    img_url=row[8],
+                    item_type=row[9],
+                    type="NONE",
+                    item_id=row[10],
+                    average=self.cache[(row[9], row[10], row[13])]['average'],
+                    low_price=self.cache[(row[9], row[10], row[13])]['price_20'],
+                    high_price=self.cache[(row[9], row[10], row[13])]['price_80'],
+                )
+            else:
+                yield scrapy.Request(
+                    url=f"https://dev-api.macguider.io/price/deal/{row[9]}/{row[10]}?unused={'true' if row[13] else 'false'}",
+                    callback=self.parse, meta={'db_item': row})
 
     def parse(self, response, **kwargs):
         item = response.meta['db_item']
         average = json.loads(response.text)['average']
         low_price = json.loads(response.text)['price_20']
         high_price = json.loads(response.text)['price_80']
+        self.cache[(item[9], item[10], item[13])] = {
+            'average': average,
+            'price_20': low_price,
+            'price_80': high_price,
+        }
 
         yield DBItemPrice(
             id=item[0],
@@ -54,6 +78,7 @@ class DBHotDealSpider(scrapy.Spider):
             url=item[7],
             img_url=item[8],
             item_type=item[9],
+            type="NONE",
             item_id=item[10],
             average=average if average else 0,
             low_price=low_price if low_price else 0,
