7dfe38bbb93519f655df6d2660ab5a13cfee34ea
황규도 <hgdkim2@naver.com>
2023-07-23 14:42:29 +0900

    chore: Update naver-cafe prefix words

--- scrapy_crawler/web_crawler/pipelines.py
+++ scrapy_crawler/web_crawler/pipelines.py
@@ -42,9 +42,11 @@ class ContentScraperPipeline:
 
         content = BeautifulSoup("\n".join(selector.css(".se-text-paragraph > span").getall())).get_text() \
             .replace("​", "") \
-            .replace("👆중고나라 앱이 있다는 걸 아시나요? 상단 중고나라 앱 다운받기 클릭!", "") \
+            .replace("👆중고나라 앱이 있다는 걸 아시나요?", "") \
+            .replace("상단 중고나라 앱 다운받기 클릭!", "") \
             .replace("👆앱에서 구매를 원하는 댓글이 달릴 수도 있어요! 더보기 클릭하고 미리 알아두기!", "") \
             .replace("※ 등록한 게시글이 회원의 신고를 받거나 이상거래로 모니터링 될 경우 중고나라 사기통합조회 DB로 수집/활용될 수 있습니다.", "") \
+            .replace("※ 유튜브, 블로그, 인스타그램 등 상품 정보 제공 목적 링크 가능(외부 거래를 유도하는 링크 제외) ", "") \
             .replace("─", "").replace("\n\n", "")
 
         images = selector.css(".se-image-resource::attr(src)").getall()
