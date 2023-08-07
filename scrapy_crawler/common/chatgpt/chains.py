54d8598e95cf13e5c270b74465d18daea55ceca7
황규도 <hgdkim2@naver.com>
2023-08-07 16:05:00 +0900

    Revert "feat: Change GPT model to 3.5-turbo"
    This reverts commit 3de613b0386a6029ecc1eb2a435cf343da08add4.

--- scrapy_crawler/common/chatgpt/chains.py
+++ scrapy_crawler/common/chatgpt/chains.py
@@ -22,7 +22,7 @@ PREFIX = "scrapy_crawler/common/chatgpt/prompts/%s"
 GPT4_MODEL_NAME = "gpt-4-0613"
 GPT3_MODEL_NAME = "gpt-3.5-turbo-0613"
 unused_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "unused.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "unused.txt", ["title", "content"]
 )
 apple_care_plus_chain: LLMChain = create_llm_chain(
     GPT3_MODEL_NAME, PREFIX % "apple_care_plus.txt", ["title", "content"]
@@ -36,19 +36,19 @@ macbook_chain: LLMChain = create_llm_chain(
     GPT3_MODEL_NAME, PREFIX % "macbook.txt", ["title", "content"]
 )
 macbook_air_13_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "macbook_air_13.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "macbook_air_13.txt", ["title", "content"]
 )
 macbook_pro_13_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "macbook_pro_13.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "macbook_pro_13.txt", ["title", "content"]
 )
 macbook_pro_14_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "macbook_pro_14.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "macbook_pro_14.txt", ["title", "content"]
 )
 macbook_pro_16_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "macbook_pro_16.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "macbook_pro_16.txt", ["title", "content"]
 )
 macmini_chain: LLMChain = create_llm_chain(
-    GPT3_MODEL_NAME, PREFIX % "macmini.txt", ["title", "content"]
+    GPT4_MODEL_NAME, PREFIX % "macmini.txt", ["title", "content"]
 )
 
 category_chain: LLMChain = create_llm_chain(
